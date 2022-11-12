b3a7401388cd03ed718f73eaa7ec46b08b311e08
JaehoBuildiOSApp <jaeho.jung.01@gmail.com>
2022-11-13 06:05:26 +0900

    reference, issue 정리한 파일 추가

---
+++ starbucksMock2/issues.swift
@@ -0,0 +1,17 @@
+//
+//  issues.swift
+//  starbucksMock2
+//
+//  Created by Jaeho Jung on 2022/11/13.
+//
+
+
+// MARK: Youtube
+/*
+ segmented control-1 로 관리하는 view가 있음
+ view 중 하나에는 또다른 segmented control-2 로 관리하는 view가 있음
+ 
+ segmented control-2를 전환시켰을 때, segmented control-2 내에서의 view 들이 화면 상에서 변화되지 않음
+ segmented control-1 로 관리하는 view에서 segmented control-2에서의 변화를 바로바로 반영하기 위한 코드가 필요할 것 같음
+ 
+ */
---
+++ starbucksMock2/reference.swift
@@ -0,0 +1,13 @@
+//
+//  reference.swift
+//  starbucksMock2
+//
+//  Created by Jaeho Jung on 2022/11/13.
+//
+
+
+// MARK: Youtube
+/*
+ SegmentedControl - Switch Views : Swift 3 (https://www.youtube.com/watch?v=e5nxg5NzLks&t=195s)
+ 
+ */
